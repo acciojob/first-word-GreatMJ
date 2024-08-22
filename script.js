a983b0775dfb2804aafdf4e65e886ac791580aba
Mahesh Jaybhaye <97793941+GreatMJ@users.noreply.github.com>
2024-08-22 18:47:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstWord(s) {
   // your code here
+// first trim it
+	let firstWord;
+	// first skip spaces
+	let index=0;
+	while(index<s.length&&s.charAt(index)===' '){
+		index++;
+	}
+
+	while(index<s.length&&s.charAt(index)!==' '){
+		firstWord.append(s.charAt(index));
+		index++;
+	}
+
+	return firstWord? firstWord:" ";
+	
 }
 
 // Do not change the code below
